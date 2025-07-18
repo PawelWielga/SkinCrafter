c32a4e1ff8a619cd7ea1039355107c6c40e4ae9e
PawelWielga <132073666+PawelWielga@users.noreply.github.com>
2025-07-18 16:58:33 +0200

    Rename WardrobeEyesColor component

---
+++ src/components/wardrobe-sections/WardrobeEyesColor.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './wardrobe.css';
+
+function WardrobeEyesColor(): JSX.Element {
+  return <div>{WardrobeEyesColor.name}</div>;
+}
+
+export default WardrobeEyesColor;
--- src/components/wardrobe-sections/wardrobe-eyescolor.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './wardrobe.css';
-
-function WardrobeEyescolor(): React.JSX.Element {
-  return <div>{WardrobeEyescolor.name}</div>;
-}
-
-export default WardrobeEyescolor;
