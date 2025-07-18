5669798aad35ef9c9acb5f48fc81371b81b78075
PawelWielga <132073666+PawelWielga@users.noreply.github.com>
2025-07-18 17:26:25 +0200

    chore: update ESLint configuration

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+    tsconfigRootDir: __dirname,
+    sourceType: "module",
+    ecmaFeatures: { jsx: true }
+  },
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended"
+  ],
+  settings: { react: { version: "detect" } },
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  }
+};
