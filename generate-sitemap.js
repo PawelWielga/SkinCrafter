0e7d4282809319e6a58cf2eb518aea7175198e8b
PawelWielga <132073666+PawelWielga@users.noreply.github.com>
2025-07-18 17:28:05 +0200

    Remove duplicate sitemap and document generation

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -1,6 +1,10 @@
 const Sitemap = require("react-router-sitemap").default;
 const path = require("path");
 
+// Write the generated sitemap to the `public` folder so Vite copies it to the
+// site root during the build step.
+const sitemapOutput = path.resolve(__dirname, "public", "sitemap.xml");
+
 const sitemap = new Sitemap(path.resolve(__dirname, "./src/App.tsx"))
   .build("https://skincrafter.dihor.pl")
-  .save(path.resolve(__dirname, "./public/sitemap.xml"));
+  .save(sitemapOutput);
