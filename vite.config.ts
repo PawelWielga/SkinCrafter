074562263ea55648a8f78dc3c85d8d5a5c4a45dc
PawelWielga <132073666+PawelWielga@users.noreply.github.com>
2025-07-20 16:25:59 +0200

    fix env values on build

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
   server: {
     host: true,
   },
-});
+  define: {
+    'import.meta.env.DEV': mode !== 'production',
+    'import.meta.env.PROD': mode === 'production',
+  },
+}));
