2f5ee3584a52176f142d3f3447c2fd10f082a72b
PawelWielga <132073666+PawelWielga@users.noreply.github.com>
2025-07-20 17:16:56 +0200

    Add under construction banner

---
+++ src/components/UnderConstructionBanner.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './UnderConstructionBanner.css';
+
+export default function UnderConstructionBanner(): React.JSX.Element {
+  return (
+    <div className="under-construction-banner" role="status">
+      Site under construction
+    </div>
+  );
+}
