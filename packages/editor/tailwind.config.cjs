a7ac3910e01d99a3debc1ea28f906fa76f035d70
PawelWielga <132073666+PawelWielga@users.noreply.github.com>
2026-08-19 21:52:32 +0200

    fix: disable unscoped Tailwind container utility

--- packages/editor/tailwind.config.cjs
+++ packages/editor/tailwind.config.cjs
@@ -1,7 +1,10 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   important: '.skincrafter-editor',
-  corePlugins: { preflight: false },
+  corePlugins: {
+    preflight: false,
+    container: false,
+  },
   theme: { extend: {} },
   plugins: [],
 };
